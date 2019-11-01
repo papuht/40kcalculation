d314436692fc3ab2d2b6887f777255267dbd60d5
papuht <papuht@utu.fi>
2019-11-01 19:11:14 +0200

    started developing menu

---
+++ mainmenu.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+
+top = tk.Tk()
+menubar = tk.Menu(top)
+
+file = tk.Menu(menubar, tearoff = 0)
+
+
+
+insert = tk.Menu(file, tearoff = 0)
+insert.add_command(label = "Insert units")
+insert.add_command(label = "Insert melee weapons")
+insert.add_command(label = "Insert ranged weapons")
+insert.add_command(label = "Insert factions")
+
+search = tk.Menu(file, tearoff = 0)
+search.add_command(label = "Search units")
+search.add_command(label = "Search melee weapons")
+search.add_command(label = "Search ranged weapons")
+
+
+file.add_cascade(label = "Insert into database...", menu = insert)
+file.add_cascade(label = "Search from database", menu = search)
+file.add_command(label = "Calculate results")
+
+
+file.add_separator()
+file.add_command(label = "Exitus", command = top.quit)
+
+menubar.add_cascade(label="File", menu=file) 
+top.config(menu = menubar)
+top.mainloop()  
